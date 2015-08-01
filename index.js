6434e14164b6fb5065ca7d19324728debea10c00
shivkanth <shivkanthb@gmail.com>
2015-07-31 20:42:06 -0700

    map search and google search added

--- index.js
+++ index.js
@@ -2,9 +2,22 @@
 
 
 var query = process.argv.slice(2);
+var thequery="";
+//console.log(process.argv.length);
+var googleQuerySearch=""
+if(query[0]=="-m")
+{
+	query = query.slice(1);
+	googleQuerySearch = "http://google.com/maps/search/";
+}
+else
+	googleQuerySearch = "http://google.com/search?q=";
 
-//console.log("open "+query[0]);
-
+for (i=0;i<query.length;i++)
+{
+	thequery+= query[i]+ " ";
+}
+//console.log("open "+thequery);
 
 var exec = require('child_process').exec;
-var child = exec('open ' + query[0]);
+var child = exec('opener ' + googleQuerySearch+encodeURIComponent(thequery).replace(/%20/g,'+'));
